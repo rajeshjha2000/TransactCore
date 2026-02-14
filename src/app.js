8204fe1c6fae3baf18507c60fff4f840614d09d0
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-02-14 16:15:50 +0530

    implement transaction and account controllers

--- src/app.js
+++ src/app.js
@@ -9,5 +9,19 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
+
+/**
+ * - Use Routes
+ */
+
+app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
 
 module.exports = app
