49a763423be46c5ab83ab45d258b9f52b4db749e
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-02-15 11:05:43 +0530

    add health check endpoint

--- src/app.js
+++ src/app.js
@@ -20,6 +20,10 @@ const transactionRoutes = require("./routes/transaction.routes")
  * - Use Routes
  */
 
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRoutes)
